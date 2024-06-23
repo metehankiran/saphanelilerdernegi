7871bbef0d7d55058c68c7c3ae0071f9d5609990
Metehan Kıran <metehankiran@outlook.com>
2024-06-24 00:10:30 +0300

    chore: install filament admin dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'root',
+            'email' => 'root@saphanelilerdernegi.test',
+            'password' => bcrypt('123123123'),
         ]);
     }
 }
